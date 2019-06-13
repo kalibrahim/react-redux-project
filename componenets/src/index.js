13c1c8e000a80a5164937a4630e93ce79df98353
kalibrahim <bowaleed500@gmail.com>
2019-06-13 11:21:37 +0300

    approval card children props

--- componenets/src/index.js
+++ componenets/src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Khalid"
-        timeAgo="Yesterday at 4:45PM"
-        content="Nice blog!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 6:25PM"
-        content="Love your post!!!!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Abdul"
-        timeAgo="2 days ago at 5:00AM"
-        content="WOW!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Khalid"
+          timeAgo="Yesterday at 4:45PM"
+          content="Nice blog!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 6:25PM"
+          content="Love your post!!!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Abdul"
+          timeAgo="2 days ago at 5:00AM"
+          content="WOW!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
