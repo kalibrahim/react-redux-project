8f0c0613a443b21f0c439a694db31b5834f1a915
kalibrahim <bowaleed500@gmail.com>
2019-06-13 11:07:24 +0300

    passing props

--- componenets/src/index.js
+++ componenets/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Khalid"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Abdul"/>
+      <CommentDetail
+        author="Khalid"
+        timeAgo="Yesterday at 4:45PM"
+        content="Nice blog!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 6:25PM"
+        content="Love your post!!!!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Abdul"
+        timeAgo="2 days ago at 5:00AM"
+        content="WOW!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
