758e92cb461c04e93e42ec2f146ddabb2d59cf71
kalibrahim <bowaleed500@gmail.com>
2019-06-13 12:43:24 +0300

    geoloaction

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+    );
+
     return <div>Hi there!</div>;
 };
 
