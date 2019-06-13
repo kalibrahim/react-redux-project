ebd47ed05545552a598e667c98f006b702f8d125
kalibrahim <bowaleed500@gmail.com>
2019-06-13 10:43:56 +0300

    Nesting componenets

--- componenets/src/index.js
+++ componenets/src/index.js
@@ -1,52 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail"
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="contnet">
-          <a href="/" className="author">
-            Khalid
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="contnet">
-          <a href="/" className="author">
-            {faker.name.firstName()}
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">{faker.lorem.sentence()}</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="contnet">
-          <a href="/" className="author">
-            {faker.name.firstName()}
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">{faker.lorem.sentence()}</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
   );
 };
