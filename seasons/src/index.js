e13354234b4cf44bdc856909c67e07f68d77c5b0
kalibrahim <bowaleed500@gmail.com>
2019-06-16 09:20:16 +0300

    Handle error

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
@@ -16,13 +16,21 @@ class App extends React.Component {
           //Bad practice
           //this.state.lat = position.coords.latitude
       },
-      err => console.log(err)
+      err => {
+          this.setState({ errorMessage: err.message })
+      }
     );
   }
 
   // React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+    <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+    </div>
+    );
   }
 }
 
